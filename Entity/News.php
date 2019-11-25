d4682fc50291a4c17fa844b1cb3ce2a320732899
Quentin GUILLOU <quentin.guillou@atos.net>
2019-11-25 12:00:47 +0100

    Adding Sluggable in TitleTrait

--- Entity/News.php
+++ Entity/News.php
@@ -5,7 +5,6 @@ namespace Owp\OwpNews\Entity;
 use App\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
 use Owp\OwpCore\Model as OwpCommonTrait;
-use Knp\DoctrineBehaviors\Model\Sluggable\Sluggable;
 
 /**
  * @ORM\Entity(repositoryClass="Owp\OwpNews\Repository\NewsRepository")
@@ -13,48 +12,16 @@ use Knp\DoctrineBehaviors\Model\Sluggable\Sluggable;
  */
 class News
 {
-    use Sluggable;
-
     use OwpCommonTrait\IdTrait;
     use OwpCommonTrait\TitleTrait;
     use OwpCommonTrait\ContentTrait;
     use OwpCommonTrait\AuthorTrait;
     use OwpCommonTrait\PrivateTrait;
+    use OwpCommonTrait\PromoteTrait;
 
-    /**
-     * @ORM\Column(type="string", length=512)
-     */
-    protected $slug;
-
-    /**
-     * @ORM\Column(name="promote", type="boolean")
-     */
-    protected $promote;
 
     public function __construct()
     {
         $this->setPromote(true);
-    }
-
-    public function isPromote(): ?bool
-    {
-        return $this->promote;
-    }
-
-    public function setPromote(bool $promote): self
-    {
-        $this->promote = $promote;
-
-        return $this;
-    }
-    
-    public function getSluggableFields()
-    {
-        return [ 'id', 'title' ];
-    }
-
-    public function generateSlugValue($values)
-    {
-        return implode('-', str_replace(' ', '-', $values));
     }
 }
